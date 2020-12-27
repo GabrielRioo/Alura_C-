f56b7ba1763ea2489e7b73cae60923a0c65c5661
GabrielRioo <gabrielpaivario@gmail.com>
2020-12-27 19:00:29 -0300

    Add the new tests with loop for

---
+++ AprendendoCSharp/TesteDivisivelPor3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TesteDivisivelPor3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Divisivel por 3");
+
+            for (int contador = 1; contador <= 100; contador++)
+                if (contador % 3 == 0)
+                    Console.WriteLine(contador);
+        }
+    }
+}
---
+++ AprendendoCSharp/TesteLacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TesteLacoDeRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada Encadeada");
+
+            for (int multiplicador = 0; multiplicador <= 10; multiplicador++)
+            {
+                for (int contador = 0; contador <= 10; contador++)
+                {
+                    Console.WriteLine(multiplicador + " * " + contador + " = " + multiplicador * contador);
+                    Console.WriteLine();
+                }
+                Console.WriteLine("=================================");
+            }
+        }
+    }
+}
