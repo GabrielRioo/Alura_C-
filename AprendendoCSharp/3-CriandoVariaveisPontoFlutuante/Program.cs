b6f800fc905c6e9cc8f381a92f5fc274791dca7b
GabrielRioo <gabrielpaivario@gmail.com>
2020-12-26 22:47:15 -0300

    add the project 2, 3, 4

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escutando projeto 3 - Criando variaveis ponto flutuante");
+            double salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade = 15.0 / 2;
+            Console.WriteLine(idade);
+        }
+    }
+}
---
+++ AprendendoCSharp/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario = 1200.50;
+
+            int salarioEmInteiro = (int)salario; //  cast
+
+            Console.WriteLine(salarioEmInteiro);
+        }
+    }
+}
