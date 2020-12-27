82142d3e96d0f6d7a8cd6b709935deb1704bda08
GabrielRioo <gabrielpaivario@gmail.com>
2020-12-27 18:28:40 -0300

    add the project 13 - break

--- AprendendoCSharp/a13-ForEncadeado/Program.cs
+++ AprendendoCSharp/a13-ForEncadeado/Program.cs
@@ -8,6 +8,36 @@ namespace a13_ForEncadeado
         {
             Console.WriteLine("Executando projeto 13 - For Encadeado");
 
+            string asterisco = "*";
+
+            //Forma diferente de fazer o desenho dos asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                Console.WriteLine(asterisco);
+                asterisco = asterisco + "*";
+            }
+
+            //Escrevendo asterisco com o BREAK
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+
+                    if (contadorColuna == contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            //Forma diferente de fazer o desenho dos asteriscos
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
         }
     }
 }
