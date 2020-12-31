3f595d0010789005aabbc3959dfa05b8e1f5657f
GabrielRioo <gabrielpaivario@gmail.com>
2020-12-31 18:18:04 -0300

    Adding project 03, 04 and 05

---
+++ Parte 2 - Orientacao a Objetos/ByteBank/04-ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCamila = new ContaCorrente();
+            contaCamila.titular = "Cammila";
+
+            Console.WriteLine(contaCamila.saldo);
+            bool resultadoSaque = contaCamila.Sacar(50);
+            Console.WriteLine(contaCamila.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contaCamila.Depositar(500);
+            Console.WriteLine(contaCamila.saldo);
+
+            ContaCorrente contaGabriel = new ContaCorrente();
+
+            contaGabriel.titular = "Gabriel";
+
+            Console.WriteLine("Saldo da Camila: " + contaCamila.saldo);
+            Console.WriteLine("Saldo da Gabriel: " + contaGabriel.saldo);
+
+            contaCamila.Transferir(200, contaGabriel);
+
+            bool resultadoTrans = contaCamila.Transferir(200, contaGabriel);
+
+            Console.WriteLine(resultadoTrans);
+            Console.WriteLine("Saldo da Camila: " + contaCamila.saldo);
+            Console.WriteLine("Saldo da Gabriel: " + contaGabriel.saldo);
+
+
+        }
+    }
+}
