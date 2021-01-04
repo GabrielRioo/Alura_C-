e3a7d93e1f05909f3bed82d83f6b647966a9ba6a
GabrielRioo <gabrielpaivario@gmail.com>
2021-01-04 14:34:21 -0300

    add project 06 - connstructor get and set

---
+++ Parte 2 - Orientacao a Objetos/ByteBank/06-ByteBank/ContaCorrente.cs
@@ -0,0 +1,78 @@
+﻿using _06_ByteBank;
+
+
+namespace _06_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo = 100.0;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        //public void DefinirSaldo(double saldo)
+        //{
+        //    if (saldo < 0)
+        //    {
+        //        return;
+        //    }
+        //    this.saldo = saldo;
+
+        //}
+
+        //public double ObterSaldo()
+        //{
+        //    return saldo;
+        //}
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+    }
+}
---
+++ Parte 2 - Orientacao a Objetos/ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Guilherme";
+            cliente.Cpf = "123.123.123-12";
+            cliente.Profissao = "Desenvolvedor";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
