223ec1938f0efeb5ea9b096091bccc04eb66861d
GabrielRioo <gabrielpaivario@gmail.com>
2021-01-06 09:22:11 -0300

    Add the first part, and an exercice.

---
+++ Parte 3 - Entendendo Heraca e Interface/ByteBank/ByteBank/Exercicios/MaiorNumero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Exercicios
+{
+    public class MaiorNumero
+    {
+        // SobreCarga utilizando o proprio metodo.
+        public double Maior(double a, double b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+        public double Maior(double a, double b, double c)
+        {
+            return Maior(Maior(a, b), c);
+        }
+    }
+}
---
+++ Parte 3 - Entendendo Heraca e Interface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+
+
+            Funcionario gabriel = new Funcionario();
+
+            gabriel.Nome = "Gabriel";
+            gabriel.CPF = "123.123.123-12";
+            gabriel.Salario = 2000;
+
+            gerenciador.Registrar(gabriel);
+
+            Diretor camila = new Diretor();
+            camila.Nome = "Camila";
+            camila.CPF = "123.123.123-32";
+            camila.Salario = 4000;
+
+            gerenciador.Registrar(camila);
+
+            Console.WriteLine(gabriel.Nome);
+            Console.WriteLine(gabriel.GetBonificacao());
+
+            Console.WriteLine(camila.Nome);
+            Console.WriteLine(camila.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+        }
+    }
+}
