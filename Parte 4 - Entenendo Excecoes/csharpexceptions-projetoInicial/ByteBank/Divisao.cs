af1eec2596af97330aa47681a9b40a94a3b76918
GabrielRioo <gabrielpaivario@gmail.com>
2021-01-10 18:35:59 -0300

    try catch with divide numbers

---
+++ Parte 4 - Entenendo Excecoes/csharpexceptions-projetoInicial/ByteBank/Divisao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class Divisao
+    {
+        public static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+
+        static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+
+        
+    }
+}
