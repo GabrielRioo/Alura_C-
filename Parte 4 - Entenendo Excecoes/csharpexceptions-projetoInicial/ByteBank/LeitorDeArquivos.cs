5e4e572361044251e1bbbfa79f53010427e252a3
GabrielRioo <gabrielpaivario@gmail.com>
2021-01-24 21:26:50 -0300

    Finally

---
+++ Parte 4 - Entenendo Excecoes/csharpexceptions-projetoInicial/ByteBank/LeitorDeArquivos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivos : IDisposable
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivos(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            // throw new FileNotFoundException();
+
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha . . .");
+
+            throw new IOException(); // exceção de input ou output
+
+            return "Linha do Arquivo";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Fechando arquivo");
+        }
+    }
+}
--- Parte 4 - Entenendo Excecoes/csharpexceptions-projetoInicial/ByteBank/Program.cs
+++ Parte 4 - Entenendo Excecoes/csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,52 @@ namespace ByteBank
         {
             try
             {
-                ContaCorrente conta1 = new ContaCorrente(456,456789);
-                ContaCorrente conta2 = new ContaCorrente(789,456123);
+                CarregarContas();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Catch no metodo main");
+            }
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivos leitor = new LeitorDeArquivos("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            // ======================================================
+            //LeitorDeArquivos leitor = null;
+
+            //try
+            //{
+            //    leitor = new LeitorDeArquivos("contasl.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            ////catch (IOException)
+            ////{
+            ////    Console.WriteLine("Exceção do tipo IOException capturada e tratada!");
+            ////}
+            //finally
+            //{
+                
+            //    if(leitor != null)
+            //        leitor.Fechar();
+            //}
+
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(456, 456789);
+                ContaCorrente conta2 = new ContaCorrente(789, 456123);
 
                 //conta1.Transferir(10000, conta2);
                 conta1.Sacar(10000);
@@ -26,8 +71,7 @@ namespace ByteBank
                 Console.WriteLine("InnerException: ");
                 Console.WriteLine(ex.InnerException.Message);
                 Console.WriteLine(ex.InnerException.StackTrace);
-            }   
-            Console.ReadLine();
+            }
         }
 
         private static void Metodo()
