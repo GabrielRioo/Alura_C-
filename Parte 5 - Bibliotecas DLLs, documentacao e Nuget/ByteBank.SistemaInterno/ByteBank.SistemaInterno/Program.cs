0df7b40cc05dedc78449e4f155ea7936d7543dc2
GabrielRioo <gabrielpaivario@gmail.com>
2021-01-30 00:13:59 -0300

    add a new prroject to export dll from other project

---
+++ Parte 5 - Bibliotecas DLLs, documentacao e Nuget/ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(459, 46865);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
