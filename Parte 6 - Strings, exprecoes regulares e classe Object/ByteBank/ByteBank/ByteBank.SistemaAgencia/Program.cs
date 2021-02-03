87a8fa0946344f1721860f2fa8b13d6c00f45e8a
GabrielRioo <gabrielpaivario@gmail.com>
2021-02-02 22:49:32 -0300

    IndexOf, Substring and Remove

---
+++ Parte 6 - Strings, exprecoes regulares e classe Object/ByteBank/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaOrigem";
+
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            string valor = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem " + valor);
+
+            string valor2 = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino " + valor2);
+
+
+
+            //string testeRemocao = "primeiraParte&parteParaRemover";
+            //int indiceEComercial = testeRemocao.IndexOf('&');
+            //Console.WriteLine(testeRemocao.Remove(indiceEComercial));
+
+            Console.ReadLine();
+
+
+
+            int indice = palavra.IndexOf(nomeArgumento);
+
+            Console.WriteLine("Tamanho da string noomeArgumento: "+ nomeArgumento.Length);
+
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length +1));
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+
+            Console.WriteLine(url);
+            string argumentos  = url.Substring(indiceInterrogacao + 1); //Substring de um caracter a partir da posição 6
+            Console.WriteLine(argumentos);
+
+            
+            Console.ReadLine();
+        }
+    }
+}
