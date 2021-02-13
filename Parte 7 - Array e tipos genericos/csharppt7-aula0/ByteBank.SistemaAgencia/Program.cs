1e18533f290c666e12d0510f64970f2ac884e538
GabrielRioo <gabrielpaivario@gmail.com>
2021-02-13 20:49:57 -0300

    arrays

---
+++ Parte 7 - Array e tipos genericos/csharppt7-aula0/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array de inteiros com 5 posições
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 21;
+            idades[2] = 51;
+            idades[3] = 84;
+            idades[4] = 76;
+
+            int acumulador = 0;
+            for (int indice = 0; indice <= 4; indice++)
+            {
+
+            }
+
+            Console.WriteLine(idades[4]);
+
+            Console.ReadLine();
+        }
+    }
+}
