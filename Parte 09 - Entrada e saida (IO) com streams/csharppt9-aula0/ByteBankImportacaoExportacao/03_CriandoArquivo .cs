359384be86fd64c4f3a264cb96d6981f2931b6b0
GabrielRioo <gabrielpaivario@gmail.com>
2021-04-18 21:33:53 -0300

    stream writer

--- Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/03_CriandoArquivo .cs
+++ Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/03_CriandoArquivo .cs
@@ -10,7 +10,22 @@ namespace ByteBankImportacaoExportacao
 {
     partial class Program
     {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
 
+            using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            {
+                var contaComoString = "456,6546,656.60,Gabriel Rio";
+                var encoding = Encoding.UTF8;
+
+                var bytes = encoding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+
+            }
+
+        }
     }
 
 
--- Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
+++ Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -12,45 +12,11 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
-            var enderecoDoArquivo = "contas.txt";
-
-            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-            using (var leitor = new StreamReader(fluxoDeArquivo))
-            {
-                while (!leitor.EndOfStream)
-                {
-                    var linha = leitor.ReadLine();
-                    var contaCorrente = ConverterStringParaContaCorrente(linha);
-                    var msg = $"{contaCorrente.Titular.Nome}: Conta numero: { contaCorrente.Numero}, ag.: {contaCorrente.Agencia}, saldo: {contaCorrente.Saldo}";
-
-                    Console.WriteLine(msg);
-                }
-            }
+            CriarArquivo();
 
             Console.ReadLine();
         }
-        static ContaCorrente ConverterStringParaContaCorrente(string linha)
-        {
-            string[] campos = linha.Split(';');
-
-            var agencia = campos[0];
-            var numero = campos[1];
-            var saldo = campos[2].Replace('.', ',');
-            var nomeTitular = campos[3];
-
-            var agenciaInt = int.Parse(agencia);
-            var numeroInt = int.Parse(numero);
-            var saldoDouble = double.Parse(saldo);
-
-            var titular = new Cliente();
-            titular.Nome = nomeTitular;
-
-            var resultado = new ContaCorrente(agenciaInt, numeroInt);
-            resultado.Depositar(saldoDouble);
-            resultado.Titular = titular;
-
-            return resultado;
-        }
+       
 
     }
 } 
