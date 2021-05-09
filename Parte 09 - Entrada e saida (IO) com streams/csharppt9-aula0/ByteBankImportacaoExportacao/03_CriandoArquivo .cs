51d9113e4d1dd6a74b3545bbe37d4c7bac8b8f48
GabrielRioo <gabrielpaivario@gmail.com>
2021-05-09 20:38:17 -0300

    Add streamWriter and create a file

--- Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/03_CriandoArquivo .cs
+++ Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/03_CriandoArquivo .cs
@@ -14,7 +14,7 @@ namespace ByteBankImportacaoExportacao
         {
             var caminhoNovoArquivo = "contasExportadas.csv";
 
-            using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
             {
                 var contaComoString = "456,6546,656.60,Gabriel Rio";
                 var encoding = Encoding.UTF8;
@@ -26,6 +26,19 @@ namespace ByteBankImportacaoExportacao
             }
 
         }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                escritor.Write("456,123654,789.65,Gabriel Rio");
+            }
+
+        }
+
     }
 
 
