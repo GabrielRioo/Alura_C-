37263bfdbf26cfb775d99fe3ceddb3d6b6dfe3dc
GabrielRioo <gabrielpaivario@gmail.com>
2021-05-14 22:50:57 -0300

    File

--- Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
+++ Parte 09 - Entrada e saida (IO) com streams/csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -1,4 +1,4 @@
-﻿ using ByteBankImportacaoExportacao.Modelos;
+﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,17 +6,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ByteBankImportacaoExportacao 
-{ 
-    partial class Program 
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
     {
         static void Main(string[] args)
         {
-            UsarStreamDeEntrada();
+            //Escrever na tela
+            File.WriteAllText("escrevendo.txt", "Olá mundo");
+            Console.WriteLine("arquivvo escrevendo.txt criado");
+
+            //Le todas as linhas como bytes
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+            Console.WriteLine($"Arquivo pussui {bytesArquivo.Length} bytes");
+
+            // quando o arquivo possui poucas linhas. Le toddas as linhas
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach (var item in linhas)
+            {
+                Console.WriteLine(item);
+            }
+            //UsarStreamDeEntrada();
             Console.ReadLine();
         }
-       
+
 
     }
-} 
- 
+}
