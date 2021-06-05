729d52c27553bec381385df29cdbd2184f5f5dfe
GabrielRioo <gabrielpaivario@gmail.com>
2021-06-04 21:19:09 -0300

    initial project casa do codigo

--- Parte 12 - ASP NET Core parte 1 - E-commerce/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ Parte 12 - ASP NET Core parte 1 - E-commerce/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -1,15 +1,13 @@
-using System;
+ï»¿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-namespace mvc
+namespace CasaDoCodigo
 {
     public class Startup
     {
@@ -23,34 +21,29 @@ namespace mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles(); // faz com que a aplicação trabalhe com arquivos estaticos (imagens, css...)
 
-            app.UseRouting();
+            app.UseStaticFiles();
 
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
