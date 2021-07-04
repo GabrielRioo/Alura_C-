e67fc9be81c03b8047cb10b9afc1ba81b956fc11
GabrielRioo <gabrielpaivario@gmail.com>
2021-07-03 21:15:11 -0300

    Adicionando produtos no banco de dados

--- Parte 12 - ASP NET Core parte 1 - E-commerce/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/DataService.cs
+++ Parte 12 - ASP NET Core parte 1 - E-commerce/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
@@ -20,6 +21,13 @@ namespace CasaDoCodigo
 
             var json = File.ReadAllText("livros.json");
             var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+
+            foreach (var item in livros)
+            {
+                contexto.Set<Produto>().Add(new Produto(item.Codigo, item.Nome, item.Preco)); // adiciona informações na memoria, e nao pro banco de dados.
+
+            }
+            contexto.SaveChanges(); // adiciona no banco de dados.
         }
     }
 
