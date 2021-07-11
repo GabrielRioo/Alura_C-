0d6671e3abc8ebbd2508bab9b11d6b2bec2cfaaa
GabrielRioo <gabrielpaivario@gmail.com>
2021-07-10 21:14:04 -0300

    Usando linq select para recuperar um arquivo do banco

--- Parte 13 - Entity Framework Core Banco de Dados/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ Parte 13 - Entity Framework Core Banco de Dados/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -5,17 +5,29 @@ namespace Alura.Loja.Testes.ConsoleApp
 {
     public class LojaContext : DbContext
     {
+        public LojaContext()
+        {
+        }
+        public LojaContext(DbContextOptions<LojaContext> options) : base(options)
+        {
+        }
+
         // classes que vai ser persistida pelo entity
         // DbSet - representa o conjunto de objetos da classe
         // Produtos = nome da tabela no banco de dados
         public DbSet<Produto> Produtos { get; set; }
 
+
+
         // definir o banco de dados e onde ele fica - Evento de configuração
         // sobreeescrever um metodo da classe DbContext
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // construtor de opçoes
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+            if (!optionsBuilder.IsConfigured)
+            {
+                // construtor de opçoes
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+            }
         }
     }
 }
