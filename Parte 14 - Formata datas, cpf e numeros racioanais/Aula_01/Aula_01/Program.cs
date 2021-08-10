e003290f47d44e07dd58b460ab6ab5ae26014749
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-09 22:48:30 -0300

    Inicializando projeto - Validacao de cpf com o pacote Caelum.Stella

---
+++ Parte 14 - Formata datas, cpf e numeros racioanais/Aula_01/Aula_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using Caelum.Stella.CSharp.Validation;
+using System;
+using System.Diagnostics;
+
+namespace ValidadorDocumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 86288366757
+            // 98745366797
+            // 22222222222
+
+            string cpf1 = "86288366757";
+            string cpf2 = "98745366797";
+            string cpf3 = "22222222222";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine($"CPF valido: {cpf}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"CPF invalido: {cpf} - {ex.Message}");
+            }
+        }
+    }
+}
