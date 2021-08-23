1c921d4fc9818c23ca87c231885e882081872ecf
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-22 21:12:47 -0300

    Extraindo a classe de javascript

---
+++ Parte 12 - ASP NET Core parte 1 - E-commerce/CasaDoCodigo/Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -0,0 +1,18 @@
+﻿function clickIncremento(btn) {
+    var linhaDoItem = $(btn).parents('[item-id]');
+    var itemId = $(linhaDoItem).attr('item-id');
+    var novaQtde = $(linhaDoItem).find('input').val();
+
+    var data = {
+        Id: itemId,
+        Quantidade: novaQtde,
+    }
+    $.ajax({
+        url: '/pedido/updateQuantidade',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data)
+    });
+
+    debugger;
+}
